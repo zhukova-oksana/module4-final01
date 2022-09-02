703b98ffbc20baa70096e4a031128299f7ab2d38
zhukova_oksana <ksy2905@gmail.com>
2022-09-02 14:47:13 +0300

    edit error game

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const marbles = (inLine = 1, balls = [5, 5]) => {
+const marbles = (balls = [5, 5]) => {
   let isGameOver = false;
   const players = ['Компьютер', 'Вы'];
   const ballsWords = ['шар', 'шара', 'шаров'];
@@ -31,16 +31,18 @@ const marbles = (inLine = 1, balls = [5, 5]) => {
   };
 
 
-  const bet = askNum(balls[0]);
+  const bet = askNum(balls[1]);
 
   if (bet === null) return alert('Игра окончена!');
 
-  const guess = thinkNum(balls[inLine]) % 2;
+  const guess = thinkNum(balls[1]) % 2;
 
-  const winner = +bet % 2 === guess ? +!inLine : inLine;
+  const winner = +bet % 2 === guess ? 1 : 0;
+
 
   const result = balls.map((playerBalls, index) => {
-    const result = playerBalls + (index === winner ? 1 : -1) * bet;
+    const result = playerBalls + (index === +winner ? -1 : 1) * bet;
+    console.log(result);
     isGameOver = true;
     switch (true) {
       case result <= 0: return 0;
@@ -48,16 +50,17 @@ const marbles = (inLine = 1, balls = [5, 5]) => {
       default:
         isGameOver = false;
     }
+
     return result;
   });
-
-  alert(`${players[1]} поставили ${declinationWork(bet, ballsWords)}. ${players[0]} ${inLine === winner ? `` : `не `}угадал.
+  console.log('result', result);
+  alert(`${players[1]} поставили ${declinationWork(bet, ballsWords)}. ${players[0]} ${winner ? `` : `не `}угадал.
       СЧЕТ:
       ${result.map((playerBalls, index) => `${['У компьютера', '\n      У Вас'][index]}: ${declinationWork(playerBalls, ballsWords)}`).join('')}`);
+  console.log('winner', winner);
+  if (isGameOver && !confirm(`${winner ? `${players[+!winner]} победил!\n` : `${players[+!winner]} победили!`} \nХотите сыграть еще?`)) return;
 
-  if (isGameOver && !confirm(`${winner ? `${players[winner]} победили!\n` : `${players[winner]} победил`} \nХотите сыграть еще?`)) return;
-
-  return marbles(...(isGameOver ? [] : [+!inLine, result]));
+  return marbles(...(isGameOver ? [] : [result]));
 };
 
 marbles();
